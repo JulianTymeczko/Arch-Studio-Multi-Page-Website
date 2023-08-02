5c7cfc51c4b5042f461aff430e0aa843e847accd
Julian Houle-T <jhouletymeczko@gmail.com>
2023-08-02 06:27:47 -0400

    added mobile navigation to the portfolio page

---
+++ scripts/portfolio.js
@@ -0,0 +1,65 @@
+// this is for mobile navigation ==========================================================
+
+function disableScroll() {
+   
+    const scrollY = window.scrollY;
+
+   
+    document.body.style.position = 'fixed';
+    document.body.style.top = `-${scrollY}px`;
+}
+
+function enableScroll() {
+    
+    const scrollY = parseInt(document.body.style.top || '0', 10);
+
+  
+    document.body.style.position = '';
+    document.body.style.top = '';
+    window.scrollTo(0, -scrollY);
+}
+
+
+let mobileNavigationList = document.querySelector(".mobile-navigation-list")
+let mobileNavButton = document.getElementById("mobile-hamburger-button")
+mobileNavButton.addEventListener("click", function () {
+
+    if (mobileNavigationList.style.display == "none" || !mobileNavigationList.style.display) {
+        mobileNavigationList.style.display = "flex"
+        header.setAttribute("style", "background-image: url(./assets/home/mobile/mobile-really-dark-image-hero-paramour.jpg)")
+        headerA.setAttribute("style", "filter: brightness(80%);")
+        headerP.setAttribute("style", "filter: brightness(80%);")
+        disableScroll()
+       
+        
+    }
+    else if (mobileNavigationList.style.display == "flex") {
+        mobileNavigationList.style.display = "none"
+        header.setAttribute("style", "background-image: url(./assets/home/mobile/dark-image-hero-paramour.jpg)")
+        headerA.setAttribute("style", "filter: brightness(100%);")
+        headerP.setAttribute("style", "filter: brightness(100%);")
+        enableScroll()
+    }
+
+})
+
+
+window.addEventListener('resize', function() {
+    const windowWidth = window.innerWidth;
+    if (mobileNavigationList.style.display == "flex" && windowWidth >= 768){
+        mobileNavigationList.style.display = "none"
+        header.setAttribute("style", "background-image: url(./assets/home/tablet/dark-image-hero-paramour-tablet.jpg)")
+        headerA.setAttribute("style", "filter: brightness(100%);")
+        headerP.setAttribute("style", "filter: brightness(100%);")
+        enableScroll()
+    }
+    else if (windowWidth >= 768){
+        header.setAttribute("style", "background-image: url(./assets/home/tablet/dark-image-hero-paramour-tablet.jpg)")
+    }
+    if (windowWidth < 768 && mobileNavigationList.style.display == "none"){
+        header.setAttribute("style", "background-image: url(./assets/home/mobile/dark-image-hero-paramour.jpg)")
+    }
+    if (windowWidth >= 1440){
+        header.setAttribute("style", "background-image: url(./assets/home/desktop/dark-paramour-tower.jpg)")
+    }
+});
