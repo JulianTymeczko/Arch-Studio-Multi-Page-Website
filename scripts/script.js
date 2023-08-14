0328fc404a9701d7f0e3ead83128368deef4e909
Julian Houle-T <jhouletymeczko@gmail.com>
2023-08-13 20:09:49 -0400

    fixed bug involving images on homepage

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 // this is for mobile navigation =====================
 function disableScroll() {
-   
+
     const scrollY = window.scrollY;
 
-   
+
     document.body.style.position = 'fixed';
     document.body.style.top = `-${scrollY}px`;
 }
 
 function enableScroll() {
-    
+
     const scrollY = parseInt(document.body.style.top || '0', 10);
 
-  
+
     document.body.style.position = '';
     document.body.style.top = '';
     window.scrollTo(0, -scrollY);
@@ -65,7 +65,7 @@ buttonTwo.addEventListener("click", function () {
     buttonFour.setAttribute("style", "background-color: white; color: hsl(223, 7%, 53%)")
     headerH1.innerHTML = "Seraph <br> Station"
     headerP.innerHTML = " The Seraph Station project challenged us to design a <br> unique station that would transport people through time.<br> The result is a fresh and futuristic model inspired by space <br> stations."
-    header.setAttribute("style", "background-image: url(./assets/home/desktop/dark-image-hero-seraph.jpg)")
+    header.setAttribute("style", "background-image: url(./assets/home/desktop/real-seraph-station.png)")
 })
 
 
@@ -77,7 +77,7 @@ buttonThree.addEventListener("click", function () {
     buttonFour.setAttribute("style", "background-color: white; color: hsl(223, 7%, 53%)")
     headerH1.innerHTML = " Federal II <br> Tower"
     headerP.innerHTML = " A sequel theme project for a tower originally built in the <br> 1800s. We achieved this with a striking look of brutal <br> minimalism with modern touches."
-    header.setAttribute("style", "background-image: url(./assets/home/desktop/dark-image-hero-federal.jpg)")
+    header.setAttribute("style", "background-image: url(./assets/home/desktop/real-federal-tower.png)")
 })
 
 
@@ -90,7 +90,7 @@ buttonFour.addEventListener("click", function () {
     buttonOne.setAttribute("style", "background-color: white; color: hsl(223, 7%, 53%)")
     headerH1.innerHTML = "Trinity Bank <br> Tower"
     headerP.innerHTML = " Trinity Bank challenged us to make a concept for a 84 story <br> building located in the middle of a city with a high <br> earthquake frequency. For this project we used curves to <br> blend design and stability to meet our objectives."
-    header.setAttribute("style", "background-image: url(./assets/home/desktop/dark-image-hero-trinity.jpg)")
+    header.setAttribute("style", "background-image: url(./assets/home/desktop/real-trinity.png)")
 })
 
 
@@ -109,8 +109,8 @@ mobileNavButton.addEventListener("click", function () {
         headerA.setAttribute("style", "filter: brightness(80%);")
         headerP.setAttribute("style", "filter: brightness(80%);")
         disableScroll()
-       
-        
+
+
     }
     else if (mobileNavigationList.style.display == "flex") {
         mobileNavigationList.style.display = "none"
@@ -123,25 +123,33 @@ mobileNavButton.addEventListener("click", function () {
 })
 
 
-window.addEventListener('resize', function() {
+window.addEventListener('resize', function () {
     const windowWidth = window.innerWidth;
     // const headerImg = header.style.backgroundImage
     // console.log(headerImg)
-    if (mobileNavigationList.style.display == "flex" && windowWidth >= 768){
+    if (mobileNavigationList.style.display == "flex" && windowWidth >= 768) {
         mobileNavigationList.style.display = "none"
         header.setAttribute("style", "background-image: url(./assets/home/tablet/tablet-paramour-real.png)")
         headerA.setAttribute("style", "filter: brightness(100%);")
         headerP.setAttribute("style", "filter: brightness(100%);")
         enableScroll()
     }
-    else if (windowWidth >= 768){
+    else if (windowWidth >= 768) {
         header.setAttribute("style", "background-image: url(./assets/home/tablet/tablet-paramour-real.png)")
     }
-    if (windowWidth < 768 && mobileNavigationList.style.display == "none"){
+    if (windowWidth < 768 && mobileNavigationList.style.display == "none") {
         header.setAttribute("style", "background-image: url(./assets/home/mobile/real-paramour-mobile.png)")
     }
-    if (windowWidth >= 1440){
+    if (windowWidth >= 1440) {
         header.setAttribute("style", "background-image: url(./assets/home/desktop/real-paramour.png)")
+        buttonOne.setAttribute("style", "background-color: hsl(225, 13%, 12%); color: hsl(230, 21%, 95%)")
+        buttonTwo.setAttribute("style", "background-color: white; color: hsl(223, 7%, 53%)")
+        buttonThree.setAttribute("style", "background-color: white; color: hsl(223, 7%, 53%)")
+        buttonFour.setAttribute("style", "background-color: white; color: hsl(223, 7%, 53%)")
+        headerH1.innerHTML = "Project <br> Paramour"
+        headerP.innerHTML = " Project made for an art museum near Southwest London. <br>Project Paramour is a statement of bold, modern <br> architecture."
+        header.setAttribute("style", "background-image: url(./assets/home/desktop/real-paramour.png)")
+
     }
 });
 
